cf305d4ec02c9c1e3466778b0d53af4e6e6f624f
inz2020 <>
2021-08-06 19:02:01 +0200

    first commit

---
+++ src/test/java/fr/gime/issakzei/custumerservice/CustumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gime.issakzei.custumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustumerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
